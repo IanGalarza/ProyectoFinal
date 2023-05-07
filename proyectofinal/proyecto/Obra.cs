e57e7f03251a99064dfab992f650040500058089
IanGalarza <132719915+IanGalarza@users.noreply.github.com>
2023-05-07 18:42:42 -0300

    Update Obra.cs

--- proyectofinal/proyecto/Obra.cs
+++ proyectofinal/proyecto/Obra.cs
@@ -1,11 +1,4 @@
-﻿/*
- * Creado por SharpDevelop.
- * Usuario: ianga
- * Fecha: 04/05/2023
- * Hora: 15:53
- * 
- * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
- */
+
 using System;
 
 namespace proyecto
