b4f806443a723b9b7790afc1c63011e20f1f988f
IanGalarza <132719915+IanGalarza@users.noreply.github.com>
2023-05-09 11:15:55 -0300

    Update Obra.cs

--- proyectofinal/proyecto/Obra.cs
+++ proyectofinal/proyecto/Obra.cs
@@ -5,8 +5,70 @@ namespace proyecto
 {
 	public class Obra
 	{
-		public Obra()
-		{
+		//atributos
+		private double avance = 0;
+		private string tipoDeObra;
+		private string tiempoEstimado;
+		private int gruposTrabajando;
+		private int costo;
+		private string nombrePropietario;
+		private int dniPropietario;
+		private int codigoInterno;
+		
+		//constructores
+		public Obra(){}
+		public Obra (double avance, string tipoDeObra, string tiempoEstimado, int gruposTrabajando, int costo, string nombrePropietario, int dniPropietario, int codigoInterno){
+			this.avance = avance;
+			this.tipoDeObra = tipoDeObra;
+			this.tiempoEstimado = tiempoEstimado;
+			this.gruposTrabajando = gruposTrabajando;
+			this.costo = costo;
+			this.nombrePropietario = nombrePropietario;
+			this.dniPropietario = dniPropietario;
+			this.codigoInterno = codigoInterno;
+		}
+		
+		//propiedades
+		
+		public string TipoDeObra{
+			set{ tipoDeObra = value;}
+			get{ return tipoDeObra;}
+		}
+		
+		public string TiempoEstimado{
+			set{ tiempoEstimado = value;}
+			get { return tiempoEstimado;}
+		}
+		
+		public int Costo{
+			set { costo = value;}
+			get { return costo;}
+		}
+		
+		public string NombrePropietario{
+			set {nombrePropietario = value;}
+			get {return nombrePropietario;}
+		}
+		
+		public int DniPropietario{
+			set {dniPropietario = value;}
+			get {return dniPropietario;}
+		}
+		
+		public int GruposTrabajando{
+			set {gruposTrabajando = value;}
+			get {return gruposTrabajando;}
+		}
+		
+		public int CodigoInterno{
+			set {codigoInterno = value;}
+			get {return codigoInterno;}
+		}
+		
+		//metodo
+		
+		public double avanzarObra (int valor){
+			return avance += valor;
 		}
 	}
 }
